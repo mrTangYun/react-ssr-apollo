82fcbec57aa461b6fe201b5c3b0c6eff0050bb56
唐云 <tangyun@speedto.cn>
2018-07-03 23:04:46 +0800

    链接

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 const Home = () => {
     return (
          <div>
             <div>This is Home Component3</div>
+             <Link to={'/exchangeRates'}>
+                 路径
+             </Link>
             <button onClick={() => console.log('Hi there!')}>Press Me!</button>
             </div>
         );
