782b7eca99df93f66eadf4e27a28a5d6eb2f25b9
唐云 <tangyun@speedto.cn>
2018-07-03 17:20:46 +0800

    Create a apollo client

--- src/client/client.js
+++ src/client/client.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+    uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
+client
+.query({
+    query: gql`
+        {
+            rates(currency: "USD") {
+                currency
+            }
+        }
+    `
+})
+.then(result => console.log(result));
 
 ReactDOM.hydrate(
     <BrowserRouter>
