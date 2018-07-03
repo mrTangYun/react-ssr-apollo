2c2f444aea4c16cc8a484ebfa6e312955b3c0719
唐云 <tangyun@speedto.cn>
2018-07-04 00:07:57 +0800

    安装代理

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+// import proxy from 'express-http-proxy';
 const app = express();
+const proxy = require('express-http-proxy');
 
 app.use(express.static('public'));
+
+const apiPath = 'https://dev-gateway-v2.youbtrip.com/gql';
+app.use('/gql', (req, res, next) => {
+    if (req.headers.origin === 'http://www.youbtrip.com' || req.headers.origin === 'https://www.youbtrip.com') {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Credentials', 'true');
+    }
+    proxy('https://dev-gateway-v2.youbtrip.com/gql' + '/wx', {
+        proxyReqPathResolver: req => apiPath + '/wx'
+    })(req, res, next);
+});
 
 app.get('*', async (req, res) => {
     res.status(200);
